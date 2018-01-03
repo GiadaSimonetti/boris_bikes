e04ae0eb015e093778f8fbc443269bd2cf660dda
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-03 17:41:59 +0000

    Raise an error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,12 +3,11 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
   def release_bike
-    Bike.new
+    raise 'No bikes available' unless @bike
+    @bike
   end
   def dock(bike)
     @bike = bike
   end
- # def bike
- #   @bike
- # end
+
 end
